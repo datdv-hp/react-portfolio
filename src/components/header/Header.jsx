a39d4a46ce8ac3eb9a0f2b5358829128f6d9cacf
dvdat <d.v.dat160901@gmail.com>
2022-11-22 01:04:55 +0700

    fix mobile layout

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,8 +7,8 @@ import HeaderSocial from './HeaderSocial';
 
 const Header = () => {
   return (
-    <header>
-      <div id='header' className='container header__container'>
+    <header id='header'>
+      <div className='container header__container'>
         <h5>Hello I'm</h5>
         <h1>Đỗ Văn Đạt</h1>
         <h5 className='text-light'>Web Developer</h5>
